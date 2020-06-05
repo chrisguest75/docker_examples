cae5619bf969ca258985f85e007ca23fc411cf9c
Chris Guest <chris.guest@condenastint.com>
2020-06-05 10:43:19 +0100

    Add readme updates

--- 36_layers_speed/time-it.sh
+++ 36_layers_speed/time-it.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
+# Can't really use pipefail on a dotsourced script.
 #set -ef -o pipefail
 
 declare -g time_samples=()
 
+# Example function to test
 # Copy this function and then dot source the time-it sript
 : <<'COMMENT'
 function under_test() {
-    docker build --no-cache -f 100layers.Dockerfile -t 100layers .
+    echo "Parameters $*"
+    local sleeptime=1
+    if [[ -n $1 ]]; then
+        sleeptime=$1
+    fi
+    echo "sleep $sleeptime"
+    sleep $sleeptime
 }
+. ./time-it.sh under_test 10 2  
 COMMENT
 
 FUNCTION_TO_TEST="under_test"
@@ -38,6 +47,7 @@ function time_function() {
     start=${EPOCHREALTIME}
     $func_ptr "$@"
     end=${EPOCHREALTIME}
+    # Using BC as epoch realtime is too big and also it is floating point
     runtime=$(bc <<< "(${end} - ${start})")
     #echo "${runtime} seconds"
     time_samples+=( "$runtime" )
