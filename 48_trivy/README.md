5baa012788c17984bfb4de26bbc7aee97afa9b1a
Chris Guest <chris.guest@condenastint.com>
2021-07-10 18:23:52 +0100

    Add a trivy example

--- 48_trivy/README.md
+++ 48_trivy/README.md
@@ -1,19 +1,14 @@
 # README
-Demonstrate some examples of using `grype`.
+Demonstrate some examples of using `trivy`.
 
 For reducing number of vulnerabilities refer to:
 * Distroless [README.md](../28_distroless/README.md)  
 * Nix [README.md](https://github.com/chrisguest75/nix-examples/blob/master/README.md)  
 
-Other scanning examples:
-* Docker Scan [README.md](../45_docker_scan_process_mongo/README.md)  
-* Trivy [README.md](../48_trivy/README.md)  
-
 ## Prerequisites
 ```sh
-# install grype
-brew tap anchore/grype
-brew install grype
+# install trivy
+brew install aquasecurity/trivy/trivy
 ```
 
 ## Script to scan many and create report
@@ -24,14 +19,16 @@ brew install grype
 # open index.html in vscode and open live server
 ```
 
+
 ## Liveserver in vscode
 Use the live share extension.  
-The example uses a `./scans/out/images_docker.json` file that has to be served up from a webserver.
+The example uses a `./scans/out/images_trivy.json` file that has to be served up from a webserver.
 
 ```sh 
 # use a live server to server up pages that have resources
 code --install-extension ritwickdey.LiveServer
 ```
 
+
 # Resources 
-* grype docs [here](https://github.com/anchore/grype)  
+* trivy docs [here](https://aquasecurity.github.io/trivy/v0.18.3/installation/)  
---
+++ 48_trivy/scan.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+SCANTYPE=trivy
+
+# create folder to store the json outputs
+SCAN_FOLDER=./scans/$SCANTYPE
+if [[ ! -d $SCAN_FOLDER ]]; then
+  mkdir -p $SCAN_FOLDER
+fi 
+
+IMAGES=$(jq -r '.images[].imagepath' ./images_to_scan.json)
+while IFS=' ', read -r IMAGE
+do
+
+    OUTPUT=$(basename $IMAGE)
+    OUTPUT=$(echo $OUTPUT | sed 's/:/_/g') 
+    EXITCODE=0
+    if [[ ! -f "$SCAN_FOLDER/$OUTPUT.json" ]]; then 
+      # ************************************************
+      # Grype Scan
+      # ************************************************
+      echo -e "Performing the scan: $IMAGE"
+      trivy -f json -o "$SCAN_FOLDER/$OUTPUT.json" $IMAGE 
+      EXITCODE=$?
+    else  
+      echo "Skipping $SCAN_FOLDER/$OUTPUT.json already exists - delete file to recreate"
+    fi
+
+    # if we had an error scanning work out what it was.
+    if [[ $EXITCODE != 0 ]]; then
+      if [[ -f "$SCAN_FOLDER/$OUTPUT.json" ]]; then
+        ERROR=$(jq .error "$SCAN_FOLDER/$OUTPUT.json")
+        if [[ $ERROR != "null" ]]; then
+          echo -e "Error scanning image: '$IMAGE' - '$ERROR'"
+          #exit 2
+        fi
+      else
+          echo -e "Error scanning image: '$IMAGE' - no generated '$SCAN_FOLDER/$OUTPUT.json'"
+      fi
+    fi
+done <<< "$IMAGES"
+
+echo "Process the results './scans/out/images_trivy.json'"
+if [[ ! -d ./scans/out ]]; then
+  mkdir -p ./scans/out
+fi 
+./aggregate.sh | jq -s '{images: (.)}' > ./scans/out/images_trivy.json  
+
+
+
+
+
+
+
