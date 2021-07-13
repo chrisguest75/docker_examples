3b1db618004d6da26bd982eddb08308f10072676
Chris Guest <chris.guest@condenastint.com>
2021-07-13 16:19:38 +0100

    Remove duplicates for trivy and snyk

--- 48_trivy/aggregate.sh
+++ 48_trivy/aggregate.sh
@@ -12,7 +12,7 @@ for f in $FILES
 do
   TMP_FRAGMENT_FILE=$(mktemp)
   #echo $TMP_FRAGMENT_FILE
-  jq '[ .[].Vulnerabilities[] | {"key":.Severity | ascii_downcase, "value":.VulnerabilityID} ] | map([.] | from_entries) | reduce .[] as $o ({}; reduce ($o|keys)[] as $key (.; .[$key] += [$o[$key]] )) | {CVE:.}' $f > "$TMP_FRAGMENT_FILE"
+  jq '[ .[].Vulnerabilities[] | {"key":.Severity | ascii_downcase, "value":.VulnerabilityID} ] | map([.] | from_entries) | reduce .[] as $o ({}; reduce ($o|keys)[] as $key (.; .[$key] += [$o[$key]] )) | {low:(.low + []) | sort | unique, high:(.high + []) | sort | unique, critical:(.critical + []) | sort | unique, medium:(.medium + []) | sort | unique } | {CVE:.}' $f > "$TMP_FRAGMENT_FILE"
   TMP_IMAGE_FILE=$(mktemp)
   #echo $TMP_IMAGE_FILE
   jq --arg cluster "$CLUSTER"  -c '{ "cluster": $cluster, "issues":(.[].Vulnerabilities | group_by(.Severity) | map({"severity":.[0].Severity | ascii_downcase, "count":length})), "image":.[].imagepath, "namespace":.[].namespace, "inputfile":input_filename, "error": (if .[].error == null then "" else .[].error end)}' $f  > "$TMP_IMAGE_FILE"
