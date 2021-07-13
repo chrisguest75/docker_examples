d2d3c9de544c3cc795fa1fdd382111a796507564
Chris Guest <chris.guest@condenastint.com>
2021-07-13 14:46:51 +0100

    Add CVE grouping to docker scan example

--- 45_docker_scan_process_mongo/aggregate.sh
+++ 45_docker_scan_process_mongo/aggregate.sh
@@ -11,7 +11,7 @@ FILES="$SCAN_FOLDER/*"
 for f in $FILES
 do
   TMP_FRAGMENT_FILE=$(mktemp)
-  jq '.vulnerabilities[].identifiers["CVE"][]' $f | jq -s '. | sort_by(.) | { CVE: .}' > "$TMP_FRAGMENT_FILE"
+  jq '[ .vulnerabilities[] | {key:.severity | ascii_downcase, value:.identifiers["CVE"][]} ] | map([.] | from_entries) | reduce .[] as $o ({}; reduce ($o|keys)[] as $key (.; .[$key] += [$o[$key]] )) | {CVE:.}' $f > "$TMP_FRAGMENT_FILE"
   TMP_IMAGE_FILE=$(mktemp)
   jq --arg cluster "$CLUSTER"  -c '{ "cluster": $cluster, "issues":(.vulnerabilities | group_by(.severity) | map({"severity":.[0].severity, "count":length})), "image":.path, "inputfile":input_filename, "error": (if .error == null then "" else .error end)}' $f > "$TMP_IMAGE_FILE"
   jq -c -s '.[0] + { "CVE":.[1].CVE}' "$TMP_IMAGE_FILE" "$TMP_FRAGMENT_FILE" 
