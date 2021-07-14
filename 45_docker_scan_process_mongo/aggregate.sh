4581d63c608e101ca16c810a3ad00428069992e1
Christopher Guest <chrisguest75@users.noreply.github.com>
2021-07-14 19:10:43 +0100

    Add the packages aggregations to grype output (#33)
    * Add the packages aggregations to grype output
    
    * Trivy and docker scan outputting  packages
    
    Co-authored-by: Chris Guest <chris.guest@condenastint.com>

--- 45_docker_scan_process_mongo/aggregate.sh
+++ 45_docker_scan_process_mongo/aggregate.sh
@@ -11,8 +11,18 @@ FILES="$SCAN_FOLDER/*"
 for f in $FILES
 do
   TMP_FRAGMENT_FILE=$(mktemp)
+  #echo $TMP_FRAGMENT_FILE
   jq '[ .vulnerabilities[] | {key:.severity | ascii_downcase, value:.identifiers["CVE"][]} ] | map([.] | from_entries) | reduce .[] as $o ({}; reduce ($o|keys)[] as $key (.; .[$key] += [$o[$key]] )) | {low:(.low + []) | sort | unique, high:(.high + []) | sort | unique, critical:(.critical + []) | sort | unique, medium:(.medium + []) | sort | unique } | {CVE:.}' $f > "$TMP_FRAGMENT_FILE"
+
+  TMP_PACKAGES_FILE=$(mktemp)
+  #echo $TMP_PACKAGES_FILE
+  jq '[ .vulnerabilities[] | {"key":.identifiers["CVE"][0], "value":.name} ] | map([.] | from_entries) | reduce .[] as $o ({}; reduce ($o|keys)[] as $key (.; .[$key] += [$o[$key]] )) | {packages:.}' $f > "$TMP_PACKAGES_FILE"
+
   TMP_IMAGE_FILE=$(mktemp)
-  jq --arg cluster "$CLUSTER"  -c '{ "cluster": $cluster, "issues":(.vulnerabilities | group_by(.severity) | map({"severity":.[0].severity, "count":length})), "image":.path, "inputfile":input_filename, "error": (if .error == null then "" else .error end)}' $f > "$TMP_IMAGE_FILE"
-  jq -c -s '.[0] + { "CVE":.[1].CVE}' "$TMP_IMAGE_FILE" "$TMP_FRAGMENT_FILE" 
+  #echo $TMP_IMAGE_FILE
+  jq --arg cluster "$CLUSTER" -c '{ "cluster": $cluster, "issues":(.vulnerabilities | group_by(.severity) | map({"severity":.[0].severity, "count":length})), "image":.path, "inputfile":input_filename, "error": (if .error == null then "" else .error end)}' $f > "$TMP_IMAGE_FILE"
+
+  jq -c -s '.[0] + { "CVE":.[1].CVE, "packages":.[2].packages}' "$TMP_IMAGE_FILE" "$TMP_FRAGMENT_FILE" "$TMP_PACKAGES_FILE"
 done
+
+
--- 49_grype/aggregate.sh
+++ 49_grype/aggregate.sh
@@ -13,11 +13,15 @@ do
   TMP_FRAGMENT_FILE=$(mktemp)
   #echo $TMP_FRAGMENT_FILE
   jq '[ .matches[].vulnerability | {"key":.severity | ascii_downcase, "value":.id} ] | map([.] | from_entries) | reduce .[] as $o ({}; reduce ($o|keys)[] as $key (.; .[$key] += [$o[$key]] )) | {low:(.negligible + .low + []) | sort | unique, high:(.high + .unknown + []) | sort | unique, critical:(.critical + []) | sort | unique, medium:(.medium + []) | sort | unique } | {CVE:.}' $f > "$TMP_FRAGMENT_FILE"
+
+  TMP_PACKAGES_FILE=$(mktemp)
+  #echo $TMP_PACKAGES_FILE
+  jq '[ .matches[] | {"key":.vulnerability.id, "value":.artifact.name} ] | map([.] | from_entries) | reduce .[] as $o ({}; reduce ($o|keys)[] as $key (.; .[$key] += [$o[$key]] )) | {packages:.}' $f > "$TMP_PACKAGES_FILE"
+
   TMP_IMAGE_FILE=$(mktemp)
   #echo $TMP_IMAGE_FILE
   jq --arg cluster "$CLUSTER" -c '{ "cluster": $cluster, "issues":(.matches | group_by(.vulnerability.severity) | map({"severity":.[0].vulnerability.severity, "count":length})), "image":.source.target.userInput, "namespace":.namespace, "inputfile":input_filename, "error": (if .error == null then "" else .error end)}' $f > "$TMP_IMAGE_FILE"
-  jq -c -s '.[0] + { "CVE":.[1].CVE}' "$TMP_IMAGE_FILE" "$TMP_FRAGMENT_FILE" 
+
+  jq -c -s '.[0] + { "CVE":.[1].CVE, "packages":.[2].packages}' "$TMP_IMAGE_FILE" "$TMP_FRAGMENT_FILE" "$TMP_PACKAGES_FILE"
 done
-
-
 
