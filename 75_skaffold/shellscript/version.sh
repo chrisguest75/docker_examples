f9e07f8214c3c62031b55243012645ce06052524
Chris Guest <chris.guest@trint.com>
2022-08-14 14:25:49 +0100

    Start working on an example for skaffold with local docker

---
+++ 75_skaffold/shellscript/version.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CID=$(basename "$(cat /proc/1/cpuset)")
+#CID=$(cat /proc/self/cgroup | grep "cpu:/" | sed 's/\([0-9]\):cpu:\/docker\///g')
+
+env
+
+while true; do 
+    echo "$(date) '${CID}'"
+    sleep 10
+done
